80e4f384084eb4229b6b8439935de65f171351d3
Nils Ramstoeck <nilsramstoeck@gmail.com>
2022-04-06 16:13:59 +0100

    Finally got things to work properly

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: "./",
    root: 'src',
    plugins: [vue({
-      template: {
-         compilerOptions: {
-            isCustomElement: (tag) => tag.startsWith('rg-')
-         }
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => tag.startsWith('rg-')
       }
-   })]
-});
+    }
+  })]
+})
