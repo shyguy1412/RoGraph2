5978e2b0ee988bcf623a19da5ab73473d918abfb
NilsRamstoeck <nilsramstoeck@gmail.com>
2023-03-07 12:11:54 +0100

    indentation

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -14,155 +14,155 @@ const isDev = process.env.IS_DEV == "true" ? true : false;
 // });
 
 function createMenu(): Menu {
-   const menu = new Menu()
+  const menu = new Menu()
 
-   menu.append(new MenuItem({
-      label: i18n.t('File'),
+  menu.append(new MenuItem({
+    label: i18n.t('File'),
+    submenu: [
+      {
+        label: i18n.t('New'),
+        accelerator: 'ctrl+n',
+        click: menuActionMap['newSketch']
+      },
+      {
+        label: i18n.t('Open') + '...',
+        accelerator: 'ctrl+o',
+        click: menuActionMap['openSketch']
+      },
+      {
+        label: i18n.t('Examples'),
+        accelerator: 'ctrl+e',
+        click: menuActionMap['openExamples']
+      },
+      {
+        label: i18n.t('Close'),
+        click: menuActionMap['closeSketch']
+      },
+      {
+        label: i18n.t('Save'),
+        accelerator: 'ctrl+s',
+        click: menuActionMap['saveSketch']
+      },
+      {
+        label: i18n.t('Save as'),
+        accelerator: 'ctrl+shift+s',
+        click: menuActionMap['saveSketchAs']
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: i18n.t('Settings'),
+        click: menuActionMap['openSettings']
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: i18n.t('Exit'),
+        click: menuActionMap['exit']
+      },
+    ]
+  }));
+
+  menu.append(new MenuItem({
+    label: i18n.t('Edit'),
+    submenu: [
+      {
+        label: i18n.t('Undo'),
+        accelerator: 'ctrl+z',
+        click: menuActionMap['undo']
+      },
+      {
+        label: i18n.t('Redo'),
+        accelerator: 'ctrl+y',
+        click: menuActionMap['redo']
+      }, //TODO: copy/paste. Highlight selected scope?
+    ]
+  }));
+
+  menu.append(new MenuItem({
+    label: i18n.t('Sketch'),
+    submenu: [
+      {
+        label: i18n.t('Build'),
+        accelerator: 'ctrl+r',
+        click: menuActionMap['buildSketch']
+      },
+      {
+        label: i18n.t('Upload'),
+        accelerator: 'ctrl+u',
+        click: menuActionMap['uploadSketch']
+      }, //TODO: Export binary
+    ]
+  }));
+
+  //TODO:serial plotter and moniter, port selection, board selection
+  menu.append(new MenuItem({
+    label: i18n.t('Tools'),
+    submenu: [
+      {
+        label: i18n.t('Install Extension') + '...',
+        // accelerator: 'f',
+        click: menuActionMap['installExtension']
+      },
+    ]
+  }));
+
+
+  if (isDev)
+    menu.append(new MenuItem({
+      label: i18n.t('Dev'),
       submenu: [
-         {
-            label: i18n.t('New'),
-            accelerator: 'ctrl+n',
-            click: menuActionMap['newSketch']
-         },
-         {
-            label: i18n.t('Open') + '...',
-            accelerator: 'ctrl+o',
-            click: menuActionMap['openSketch']
-         },
-         {
-            label: i18n.t('Examples'),
-            accelerator: 'ctrl+e',
-            click: menuActionMap['openExamples']
-         },
-         {
-            label: i18n.t('Close'),
-            click: menuActionMap['closeSketch']
-         },
-         {
-            label: i18n.t('Save'),
-            accelerator: 'ctrl+s',
-            click: menuActionMap['saveSketch']
-         },
-         {
-            label: i18n.t('Save as'),
-            accelerator: 'ctrl+shift+s',
-            click: menuActionMap['saveSketchAs']
-         },
-         {
-            type: 'separator'
-         },
-         {
-            label: i18n.t('Settings'),
-            click: menuActionMap['openSettings']
-         },
-         {
-            type: 'separator'
-         },
-         {
-            label: i18n.t('Exit'),
-            click: menuActionMap['exit']
-         },
+        {
+          label: i18n.t('Toggle Developer Tools'),
+          accelerator: 'ctrl+shift+i',
+          click: () => { BrowserWindow.getFocusedWindow()!.webContents.toggleDevTools() }
+        }, {
+          label: i18n.t('Reload'),
+          accelerator: 'f5',
+          click: () => { BrowserWindow.getFocusedWindow()!.reload() }
+        },
+        {
+          label: i18n.t('Exit'),
+          accelerator: 'esc',
+          click: () => { app.quit() }
+        },
       ]
-   }));
+    }));
 
-   menu.append(new MenuItem({
-      label: i18n.t('Edit'),
-      submenu: [
-         {
-            label: i18n.t('Undo'),
-            accelerator: 'ctrl+z',
-            click: menuActionMap['undo']
-         },
-         {
-            label: i18n.t('Redo'),
-            accelerator: 'ctrl+y',
-            click: menuActionMap['redo']
-         }, //TODO: copy/paste. Highlight selected scope?
-      ]
-   }));
-
-   menu.append(new MenuItem({
-      label: i18n.t('Sketch'),
-      submenu: [
-         {
-            label: i18n.t('Build'),
-            accelerator: 'ctrl+r',
-            click: menuActionMap['buildSketch']
-         },
-         {
-            label: i18n.t('Upload'),
-            accelerator: 'ctrl+u',
-            click: menuActionMap['uploadSketch']
-         }, //TODO: Export binary
-      ]
-   }));
-
-   //TODO:serial plotter and moniter, port selection, board selection
-   menu.append(new MenuItem({
-      label: i18n.t('Tools'),
-      submenu: [
-         {
-            label: i18n.t('Install Extension') + '...',
-            // accelerator: 'f',
-            click: menuActionMap['installExtension']
-         },
-      ]
-   }));
-
-
-   if (isDev)
-      menu.append(new MenuItem({
-         label: i18n.t('Dev'),
-         submenu: [
-            {
-               label: i18n.t('Toggle Developer Tools'),
-               accelerator: 'ctrl+shift+i',
-               click: () => { BrowserWindow.getFocusedWindow()!.webContents.toggleDevTools() }
-            }, {
-               label: i18n.t('Reload'),
-               accelerator: 'f5',
-               click: () => { BrowserWindow.getFocusedWindow()!.reload() }
-            },
-            {
-               label: i18n.t('Exit'),
-               accelerator: 'esc',
-               click: () => { app.quit() }
-            },
-         ]
-      }));
-
-   return menu;
+  return menu;
 }
 
 function createWindow() {
 
-   // Create the browser window.
-   const width = 900;
-   const height = 700;
-   const mainWindow = new BrowserWindow({
-      width: isDev ? width + 400 : 0,
-      height: height,
-      minWidth: width,
-      minHeight: height,
-      webPreferences: {
-         preload: path.join(__dirname, 'preload.js'),
-         nodeIntegration: true,
-      },
-   });
+  // Create the browser window.
+  const width = 900;
+  const height = 700;
+  const mainWindow = new BrowserWindow({
+    width: isDev ? width + 400 : 0,
+    height: height,
+    minWidth: width,
+    minHeight: height,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    },
+  });
 
-   // and load the index.html of the app.
-   // win.loadFile("index.html");
-   mainWindow.loadURL(
-      isDev
-         ? 'http://localhost:3000'
-         : `file://${path.join(__dirname, '../src/index.html')}`
-   );
-   // Open the DevTools.
-   if (isDev) {
-      mainWindow.webContents.openDevTools();
-   }
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../src/index.html')}`
+  );
+  // Open the DevTools.
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
-   return mainWindow;
+  return mainWindow;
 }
 
 // This method will be called when Electron has finished
@@ -171,30 +171,30 @@ function createWindow() {
 
 app.whenReady().then(async () => {
 
-   await preferences.loadSettings();
-   const mainWindow = createWindow();
+  await preferences.loadSettings();
+  const mainWindow = createWindow();
 
-   mainWindow.webContents.once('did-finish-load', () => {
-      mainWindow.webContents.send('set-language', i18n.language);
-      loadExtensions()
-         .then((extensions) => {
-            // console.log(JSON.stringify(extensions));
-            extensions.forEach(extension => {
-               console.log(extension);
+  mainWindow.webContents.once('did-finish-load', () => {
+    mainWindow.webContents.send('set-language', i18n.language);
+    loadExtensions()
+      .then((extensions) => {
+        // console.log(JSON.stringify(extensions));
+        extensions.forEach(extension => {
+          console.log(extension);
 
-               mainWindow.webContents.send('load-extension', extension)
-            })
+          mainWindow.webContents.send('load-extension', extension)
+        })
 
-         });
-   })
+      });
+  })
 
 
-   app.on('activate', function () {
-      //WARN: Might be sketchy with loading of settings and stuff?
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-   })
+  app.on('activate', function () {
+    //WARN: Might be sketchy with loading of settings and stuff?
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 });
 
 
@@ -202,26 +202,26 @@ app.whenReady().then(async () => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-   if (process.platform !== 'darwin') {
-      app.quit();
-   }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 //TODO: combine all settings into one object that can be saved in one go
 ipcMain.handle('change-language', (_e, language: string) => {
-   i18n.changeLanguage(language);
-   preferences.saveSettings({
-      language
-   })
+  i18n.changeLanguage(language);
+  preferences.saveSettings({
+    language
+  })
 });
 
 i18n.on('languageChanged', () => {
-   const menu = createMenu();
-   Menu.setApplicationMenu(menu);
+  const menu = createMenu();
+  Menu.setApplicationMenu(menu);
 })
 
 ipcMain.handle('soft-reload', () => {
-   //TODO: save and restore sketch state
-   BrowserWindow.getAllWindows().forEach(window => window.close());
-   createWindow();
+  //TODO: save and restore sketch state
+  BrowserWindow.getAllWindows().forEach(window => window.close());
+  createWindow();
 })
--- src/electron/preferences.ts
+++ src/electron/preferences.ts
@@ -9,60 +9,60 @@ const dataPath = app.getPath('userData');
 const settingsFile = path.resolve(dataPath, 'settings.json');
 
 if (!fs.existsSync(settingsFile)) {
-    fs.writeFileSync(settingsFile, JSON.stringify(DefaultSettings));
+  fs.writeFileSync(settingsFile, JSON.stringify(DefaultSettings));
 }
 
 let globalSettings = loadSettingsFile();
 
 const loadSetting: any = {
-    language: loadLanguage
+  language: loadLanguage
 }
 
 async function loadSettings() {
-    const awaits: Promise<unknown>[] = [];
-    const settings = await loadSettingsFile();
+  const awaits: Promise<unknown>[] = [];
+  const settings = await loadSettingsFile();
 
-    Object.keys(settings).forEach((setting) => {
-        if (Object.keys(loadSetting).includes(setting))
-            awaits.push(loadSetting[setting](settings[<keyof Settings>setting]));
-    })
+  Object.keys(settings).forEach((setting) => {
+    if (Object.keys(loadSetting).includes(setting))
+      awaits.push(loadSetting[setting](settings[<keyof Settings>setting]));
+  })
 
-    await Promise.all(awaits);
+  await Promise.all(awaits);
 }
 
 async function loadLanguage(language: string) {
-    return i18n.changeLanguage(language);
+  return i18n.changeLanguage(language);
 }
 
 
 function loadSettingsFile() {
 
-    const buffer = fs.readFileSync(settingsFile);
-    const settings = <Settings>JSON.parse(buffer.toString())
+  const buffer = fs.readFileSync(settingsFile);
+  const settings = <Settings>JSON.parse(buffer.toString())
 
-    return settings;
+  return settings;
 }
 
 
 async function saveSettings(settings: Partial<Settings>) {
 
-    //update settings
-    Object.keys(settings).forEach(setting => {
-        globalSettings[<keyof Settings>setting] = settings[<keyof Settings>setting]!;
-    });
+  //update settings
+  Object.keys(settings).forEach(setting => {
+    globalSettings[<keyof Settings>setting] = settings[<keyof Settings>setting]!;
+  });
 
-    //save updated settings
-    return new Promise<void>((resolve) => {
-        fs.writeFile(settingsFile, JSON.stringify(globalSettings), {}, () => {
-            resolve();
-        })
-    });
+  //save updated settings
+  return new Promise<void>((resolve) => {
+    fs.writeFile(settingsFile, JSON.stringify(globalSettings), {}, () => {
+      resolve();
+    })
+  });
 }
 
 export default {
-    loadSettings,
-    saveSettings,
-    get settings() {
-        return JSON.parse(JSON.stringify(globalSettings)) as Settings;
-    }
+  loadSettings,
+  saveSettings,
+  get settings() {
+    return JSON.parse(JSON.stringify(globalSettings)) as Settings;
+  }
 }
